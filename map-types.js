40d0f0d18e9a3dc70f7a8ca6429a2d0649283803
AndroidUpscale <AndroidUpscale@users.noreply.github.com>
2019-11-15 11:42:43 +0300

    Update map-types.js

--- map-types.js
+++ map-types.js
@@ -1,19 +1,32 @@
-var baseUrl = "https://services.11arcgisonline.com/arcgis/rest/services/",
-    maps = [{
+var maps = [{
 
- base: L.tileLayer('https://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}{r}.png?apikey=7c352c8ff1244dd8b732e349e0b0fe8d'), reference: null },{
+ base: L.tileLayer('https://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}{r}.png?apikey=7c352c8ff1244dd8b732e349e0b0fe8d'), reference: null 
 
- base: L.tileLayer("http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",), reference: null },{
+},{
 
- base: L.tileLayer("https://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}{r}.png?apikey=7c352c8ff1244dd8b732e349e0b0fe8d"
-,), reference: null },{
+	 base: L.tileLayer("https://api.tiles.mapbox.com/v4/mapbox.streets-satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw"
+,), reference: null 
+ 
+ },{
+ 
+ base: L.tileLayer("https://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}{r}.png?apikey=7c352c8ff1244dd8b732e349e0b0fe8d"
+,), reference: null
+ 
+ 
+ },{
 
- base: L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",), reference: null }, {
+	 base: L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",), reference: null
+	 
+  
+ 
+ 
+ },{
 
- base: L.tileLayer("https://api.tiles.mapbox.com/v4/mapbox.streets-satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw"
-,), reference: null }, {
+     base: L.tileLayer("http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",), reference: null
+  
+ 
+ }];
 
- base: L.esri.tiledMapLayer({
-//#5فاتحة
- url: baseUrl + "Specialty/DeLorme_World_Base_Map/MapServer" }) 
-}];
+
+
+
