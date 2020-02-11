157ff8f29b0d109ae6422d1efea53f009fff68fd
AndroidUpscale <AndroidUpscale@users.noreply.github.com>
2020-02-12 01:31:44 +0300

    Add files via upload

---
+++ map-types.js
@@ -0,0 +1,148 @@
+var maps = [{
+
+ base: L.tileLayer('https://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}{r}.png?apikey=7c352c8ff1244dd8b732e349e0b0fe8d'), reference: null 
+
+},{
+	 base: L.tileLayer("https://api.mapbox.com/styles/v1/androidksa/ck3gm472008v21co0kg0a7xkr/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW5kcm9pZGtzYSIsImEiOiJjazM2cjkydHYwMzk2M25tdWUwMXI1cTNyIn0.Zv7a2Oi31HQFzps0BVOhEA",), reference: null
+ 
+ },{
+
+ base: L.tileLayer("https://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}{r}.png?apikey=7c352c8ff1244dd8b732e349e0b0fe8d"
+,), reference: null
+ 
+
+ },{
+
+	 base: L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",), reference: null
+	 
+ },{
+
+     base: L.tileLayer("http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",), reference: null
+  
+ 
+ },{
+
+     base: L.tileLayer("https://b.tile.openstreetmap.de/{z}/{x}/{y}.png",), reference: null
+  
+ 
+ },{
+
+     base: L.tileLayer("https://a.tile.opentopomap.org/{z}/{x}/{y}.png",), reference: null
+  
+ 
+},{
+
+     base: L.tileLayer("https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png",), reference: null
+  
+ 
+ },{
+
+     base: L.tileLayer("https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png",), reference: null
+  
+ 
+ },{
+
+     base: L.tileLayer("https://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}{r}.png?apikey=7c352c8ff1244dd8b732e349e0b0fe8d",), reference: null
+  
+ 
+},{
+
+     base: L.tileLayer("https://a.tile.thunderforest.com/mobile-atlas/{z}/{x}/{y}.png?apikey=7c352c8ff1244dd8b732e349e0b0fe8d",), reference: null
+  
+ 
+},{
+
+      base: L.tileLayer("https://api.mapbox.com/styles/v1/androidksa/ck68t6qg80mz71iqghyv6par2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW5kcm9pZGtzYSIsImEiOiJjazM2cjkydHYwMzk2M25tdWUwMXI1cTNyIn0.Zv7a2Oi31HQFzps0BVOhEA",), reference: null
+ 
+ 
+},{
+
+     base: L.tileLayer("https://api.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYW5kcm9pZGtzYSIsImEiOiJjazM2cjkydHYwMzk2M25tdWUwMXI1cTNyIn0.Zv7a2Oi31HQFzps0BVOhEA",), reference: null
+  
+ 
+ },{
+
+     base: L.tileLayer("https://api.tiles.mapbox.com/v4/mapbox.streets-basic/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",), reference: null
+ 
+},{
+
+      base: L.tileLayer("https://api.mapbox.com/styles/v1/androidksa/ck3gf6o2u02jc1clcp9hituzj/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW5kcm9pZGtzYSIsImEiOiJjazM2cjkydHYwMzk2M25tdWUwMXI1cTNyIn0.Zv7a2Oi31HQFzps0BVOhEA",), reference: null
+ 
+ },{
+
+      base: L.tileLayer("https://api.mapbox.com/styles/v1/androidksa/ck3jmy0eg0bms1cmwm3dxwirg/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW5kcm9pZGtzYSIsImEiOiJjazM2cjkydHYwMzk2M25tdWUwMXI1cTNyIn0.Zv7a2Oi31HQFzps0BVOhEA",), reference: null
+ 
+},{
+
+      base: L.tileLayer("https://api.mapbox.com/styles/v1/androidksa/ck3hx799819rg1cpka2vl4ro9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW5kcm9pZGtzYSIsImEiOiJjazM2cjkydHYwMzk2M25tdWUwMXI1cTNyIn0.Zv7a2Oi31HQFzps0BVOhEA",), reference: null
+ 
+},{
+
+      base: L.tileLayer("https://api.mapbox.com/styles/v1/androidksa/ck3xvlxsm0ld31co3v1dh4vwr/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW5kcm9pZGtzYSIsImEiOiJjazM2cjkydHYwMzk2M25tdWUwMXI1cTNyIn0.Zv7a2Oi31HQFzps0BVOhEA",), reference: null
+ 
+},{
+
+       base: L.tileLayer("https://api.mapbox.com/styles/v1/lukasmartinelli/cj2amg3qv001g2so47fcnb49y/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibHVrYXNtYXJ0aW5lbGxpIiwiYSI6ImNqMmFvNDF4ODAwOGczM3M1cGJoOG13Y20ifQ.2A3lELT-I1U68zoCwUnPdw",), reference: null
+ 
+ }];
+
+
+
+/**
+
+
+mapbox://styles/androidksa/ck68t6qg80mz71iqghyv6par2
+
+
+
+ mapbox.streets-basic
+ck3wq1nfo1rjl1dpke6rrxenv
+ck3gf6o2u02jc1clcp9hituzj
+
+mapbox://styles/androidksa/ck3e1uz7q4fx21cqjfmyioqq5
+mapbox.satellite
+androidksa/ck3e1uz7q4fx21cqjfmyioqq5
+داكنة وفاتحة
+ck3grnd1p0e031cpe5ui99nhj
+
+ck3gr8kyc083n1cpkugcelwup
+
+base: L.tileLayer("https://api.tiles.mapbox.com/v4/mapbox.streets-satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYW5kcm9pZGtzYSIsImEiOiJjazM2cjkydHYwMzk2M25tdWUwMXI1cTNyIn0.Zv7a2Oi31HQFzps0BVOhEA"
+,), reference: null 
+
+key=
+pk.eyJ1IjoiaHdsZWVoMiIsImEiOiJjaWxqbXZyYjU0emo3dmFtY250eWhsNXBjIn0.7q_xKPH9QyOVfoCKxIqNwA
+
+hybrid
+https://api.maptiler.com/maps/hybrid/style.json?key=q7TixBHQ267H17VnTHFw
+
+
+mapbox.streets-basic
+
+
+mapbox.outdoors
+
+mapbox.high-contrast
+ */
+
+//mapbox.mapbox-streets-v8
+
+
+
+//http://localhost:80/tile/{z}/{x}/{y}.png
+
+//https://a.tile.openstreetmap.de/{z}/{x}/{y}.png
+
+//الجغرافية OpenTopo
+//https://a.tile.opentopomap.org/{z}/{x}/{y}.png
+
+
+//Hike & Bike
+//https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png
+
+//https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png
+
+
+//https://a.tile.openstreetmap.de/{z}/{x}/{y}.png
+
+
